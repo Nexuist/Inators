c70f12368aa230fdcee1976bb3b72469f79c974a
Nexuist <nexuister@gmail.com>
2016-08-20 19:00:24 -0400

    Added CatalogCrawler

---
+++ Catalog Crawler/README.md
@@ -0,0 +1,7 @@
+### Introduction
+
+This file is mainly here for historical purposes, so I am unsure if it will work anymore. This was my first real attempt at web scraping using the BeautifulSoup library in Python. Usage is simple - just run it!
+
+### Modification
+
+The only variable you need to modify is `i` on line 8, which is the row with which to start at.
---
+++ Catalog Crawler/main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# Last Modified Oct 20, 2012
+from bs4 import BeautifulSoup
+import urllib2
+
+items = []
+i = 0
+
+while 1:
+    try:
+        info = urllib2.urlopen(
+            'http://m.roblox.com/catalog/catalogSearch?startrow=' + str(i))
+        soup = BeautifulSoup(info)
+        soup.prettify()
+        for item in soup.find_all("li"):
+            # Go through the individual items
+            # And get their info
+            # Link
+            link = item.a["href"]
+            # Image
+            if item.a.find("img"):
+                image = item.find("img")['src']
+            else:
+                image = "???"
+            # Name
+            title = item.h3.string
+            # Description
+            if item.a.find('p', {'class': 'catalog-list-description'}):
+                desc = item.a.find(
+                    'p', {'class': 'catalog-list-description'}).string
+            else:
+                desc = "???"
+
+            # Put it all in a dictionary
+            item = {
+                "link": link,
+                "image": image,
+                "name": title,
+                "desc": desc,
+            }
+            # And put it in the array
+            items.append(item)
+        i = i + 15
+        print("Moving on to row %s" % i)
+    except urllib2.HTTPError:
+        print("The next page could not be retrieved.")
+        break
+
+print items
